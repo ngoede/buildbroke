6559d23169c1b0c193a2b889b774879e81a4bd45
Nick Goede <ngoede@emoneyadvisor.com>
2018-07-12 15:29:35 -0400

    Updated config for cloudfront dist

--- blog.tf
+++ blog.tf
@@ -19,31 +19,37 @@ resource "aws_cloudfront_distribution" "blog_distribution" {
     s3_origin_config {
       origin_access_identity = "origin-access-identity/cloudfront/ABCDEFG1234567"
     }
+  }
 
-    enabled         = true
-    is_ipv6_enabled = true
+  enabled         = true
+  is_ipv6_enabled = true
 
-    default_root_object = "index.html"
+  default_root_object = "index.html"
 
-    aliases = ["www.nickgoede.com", "nickgoede.com"]
+  aliases = ["www.nickgoede.com", "nickgoede.com"]
 
-    default_cache_behavior {
-      allowed_methods  = ["GET", "HEAD"]
-      cached_methods   = ["GET", "HEAD"]
-      target_origin_id = "blogOrigin"
-
-      forwarded_values {
-        query_string = false
-
-        cookies {
-          forward = "none"
-        }
-      }
-
-      viewer_protocol_policy = "redirect-to-https"
-      min_ttl                = 31536000
-      default_ttl            = 31536000
-      max_ttl                = 31536000
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
     }
+  }
+
+  default_cache_behavior {
+    allowed_methods  = ["GET", "HEAD"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = "blogOrigin"
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+
+    viewer_protocol_policy = "redirect-to-https"
+    min_ttl                = 31536000
+    default_ttl            = 31536000
+    max_ttl                = 31536000
   }
 }
